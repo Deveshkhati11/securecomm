a2d91e685eab7482a91703008f3937f608d3b801
Devesh_khati <deveshkhati10@gmail.com>
2025-08-25 17:53:01 +0530

    1st initial commit

--- app/src/main/java/com/example/securecomm/MainActivity.kt
+++ app/src/main/java/com/example/securecomm/MainActivity.kt
@@ -1,20 +1,10 @@
-package com.example.securesomm
+package com.example.securecomm
 
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Example usage
-        val keyPair1 = CryptoUtils.generateKeyPair()
-        val keyPair2 = CryptoUtils.generateKeyPair()
-
-        val shared1 = CryptoUtils.generateSharedSecret(keyPair1.private, keyPair2.public)
-        val shared2 = CryptoUtils.generateSharedSecret(keyPair2.private, keyPair1.public)
-
-        println("Shared1: ${shared1.encoded.contentToString()}")
-        println("Shared2: ${shared2.encoded.contentToString()}")
     }
 }
